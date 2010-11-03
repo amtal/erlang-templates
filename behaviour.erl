987ce85d7783d3a769071d0592dc687198ffad95
amtal <alex.kropivny@gmail.com>
2010-11-03 02:19:13 -0700

    behaviours: yes, they really are that simple

---
+++ behaviour.erl
@@ -0,0 +1,21 @@
+%%% @doc One line blurb.
+%%%
+%%% More detailed, multi-line description.
+%%% @author JStmith <john.smith@gmail.com>
+-module(behaviour).
+
+-export([start/2]).
+
+-export([behaviour_info/0]).
+-vsn(0).
+
+
+%% @doc Short description.
+%% @spec (atom(),term())->term()
+start(Module,Args) ->
+    ?MODULE:func_arity_tuples(Args).
+
+%% @hidden behaviour
+behaviour_info() ->
+    [ {func_arity_tuples, 1}
+    ].
